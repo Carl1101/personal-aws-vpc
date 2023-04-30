f810ff9c39eab57a2bfd2691197656132dfad5d4
Carlos Vazquez <cvazquez@Carloss-MacBook-Pro.local>
2023-04-29 21:41:20 -0400

    Formating

--- vpc/backend.tf
+++ vpc/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    region   = "us-east-1"
-    key      = "network.tfstate"
-    encrypt  = "true"
+    region  = "us-east-1"
+    key     = "network.tfstate"
+    encrypt = "true"
   }
 }
